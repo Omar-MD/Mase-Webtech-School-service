90e8086bfc7f50b1a2098b6c38f496dae7938e87
Omar Duadu <a00315051@student.tus.ie>
2024-02-14 18:23:08 +0000

    created app skeleton, with html, css and eventHandlers

---
+++ src/main/java/com/tus/webtech/school_service/SchoolServiceApp.java
@@ -0,0 +1,14 @@
+package com.tus.webtech.school_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolServiceApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchoolServiceApp.class, args);
+		System.out.println("School Service Running...");
+	}
+
+}
---
+++ src/main/resources/static/js/eventHandlers.js
@@ -0,0 +1,58 @@
+
+// Application Event Handlers
+$(document).ready(function() {
+
+    // Login
+    $('#login-nav-link').on('click', function() {
+        $('#home-page').addClass('d-none');
+        $('#login-page').removeClass('d-none');
+    });
+
+    // Logout
+    $('#logout-nav-link').on('click', function() {
+        /*logout()*/
+        $('#login-nav-link').removeClass('d-none');
+        $('#logout-nav-link').addClass('d-none');
+    });
+
+    // Home Page
+    $('#logo').on('click', function() {
+        $('#home-page').removeClass('d-none');
+        $('#login-page').addClass('d-none');
+        $('#create-account-page').addClass('d-none');
+        handleSidebarNavigation("#ethos-nav-link" ,'#ethos-page');
+    });
+
+    // About Us
+    $("#ethos-nav-link").on('click', function() {
+          handleSidebarNavigation("#ethos-nav-link" ,'#ethos-page');
+    });
+
+    // Curriculm
+    $("#curriculum-nav-link").on('click', function() {
+          handleSidebarNavigation("#curriculum-nav-link", '#curriculum-page');
+    });
+
+    // Admission Process
+    $("#admission-nav-link").on('click', function() {        
+         handleSidebarNavigation("#admission-nav-link", '#admission-page');
+    });
+
+    // Tuition Fees
+    $("#tuition-nav-link").on('click', function() {
+         handleSidebarNavigation("#tuition-nav-link", '#tuition-page');
+    });
+
+    // Contact Us
+    $("#contact-nav-link").on('click', function() {
+        handleSidebarNavigation("#contact-nav-link", '#contact-page');
+    });
+});
+
+const handleSidebarNavigation = function(navID, pageID) {
+    $('.aside-link').removeClass('active');
+    $(navID).addClass('active');
+    
+    $('.main-page').addClass('d-none');
+    $(pageID).removeClass('d-none');
+}
